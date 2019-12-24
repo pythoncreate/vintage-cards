874d8db4efb675b8d61a3cd07765317379572009
Chris Stuart <crstuart@gmail.com>
2019-12-24 15:45:10 -0500

    fix

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ app.use("/testAPI", testAPIRouter);
 app.use("/getMultiple", getMultiple);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build/index.html"));
+  res.sendFile(path.join(__dirname, "/build/index.html"));
 });
 // app.use("/mwBase", mwBase);
 
